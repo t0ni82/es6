a13ced1cf711829168617cff14a1650710e1d341
Antoni Moragues <antonimoragues@gmail.com>
2025-01-10 10:30:26 +0100

    add Item version 1

--- DOM/06-crear-elementos/js/script.js
+++ DOM/06-crear-elementos/js/script.js
@@ -1,6 +1,6 @@
 //Rapida de escribir pero poco eficiente
 /*
-      <li class="item">
+      <li>
         Leche
         <button class="remove-item btn-link text-red">
           <i class="fa-solid fa-xmark"></i>
@@ -8,26 +8,32 @@
       </li>
 */
 
-//Traer una referencia al boton
-const btnAddItem = document.querySelector("#mi-boton");
 //Traer referencia del input
 const itemInput = document.querySelector("#item-input");
+//Traer una referencia al boton
+const btnAddItem = document.querySelector("#mi-boton");
 //Traer una referencia a la lista
 const lista = document.querySelector("#item-list");
 
-function createListItem(textItem) {
-  const li = document.createElement("li");
-  li.innerHTML = `${textItem}
-                  <button class="remove-item btn-link text-red">
-                          <i class="fa-solid fa-xmark"></i>
-                  </button>`;
-  lista.appendChild(li);
-  console.log(li);
-}
-
 //Event Listeners
 btnAddItem.addEventListener("click", () => {
-  console.log("Añadir item!!!");
-  createListItem(itemInput.value);
-  itemInput.value = "";
+  console.log("Añadir item!!!", itemInput.value);
+  console.time("version-A");
+  createListItem1(itemInput.value);
+  console.timeEnd("version-A");
 });
+
+//funciones
+//Version senzilla pero no demasiado limpia
+function createListItem1(textItem) {
+  let listaTextoHtml = lista.innerHTML;
+  listaTextoHtml += `<li>${textItem}
+                           <button class="remove-item btn-link text-red">
+                              <i class="fa-solid fa-xmark"></i>
+                          </button>
+                      </li>`;
+  lista.innerHTML = listaTextoHtml;
+}
+
+//Limpia y más rendimiento
+function createListItem2(textItem) {}
