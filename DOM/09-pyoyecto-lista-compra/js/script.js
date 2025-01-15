f5d1f7fc949f48744032e54d5a194c13eb07b301
Antoni Moragues <antonimoragues@gmail.com>
2025-01-15 10:33:05 +0100

    displayItems

--- DOM/09-pyoyecto-lista-compra/js/script.js
+++ DOM/09-pyoyecto-lista-compra/js/script.js
@@ -6,6 +6,17 @@ const btnClear = document.querySelector("#clear");
 const itemFilter = document.querySelector("#filter");
 
 /**
+ * @description Obtiene los datos del LStorage y refresca la lista
+ */
+function displayItems() {
+  const lista = getItemsFromLocalStorage();
+  lista.forEach((item) => {
+    const li = createNewItem(item);
+    itemList.appendChild(li);
+  });
+  checkUI();
+}
+/**
  *
  * @param {SubmitEvent} evt
  */
@@ -24,10 +35,44 @@ function addItem(evt) {
   const li = createNewItem(newItem);
   itemList.appendChild(li);
 
+  //Añadir el elemento al localStorage
+  addItemToLocalStorage(newItem);
+
   //Refrescamos el UI
   checkUI();
   //Limpiar el campo de texto
   itemInput.value = "";
+}
+function removeItem(evt) {
+  if (evt.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Vas ha eliminar el item")) {
+      evt.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+function clearItems() {
+  // itemList.innerHTML = "";
+  if (confirm("Vas ha eliminar toda la lista")) {
+    while (itemList.firstChild) {
+      itemList.removeChild(itemList.firstChild);
+    }
+    checkUI();
+  }
+}
+function filterItems(evt) {
+  const text = evt.target.value.toLowerCase();
+  const items = itemList.querySelectorAll("li");
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.includes(text)) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
 
 /*********     Funciones para la creación de los elementos de la lista **********/
@@ -55,41 +100,10 @@ function createIcon(clases) {
   icon.className = clases;
   return icon;
 }
-
-function removeItem(evt) {
-  if (evt.target.parentElement.classList.contains("remove-item")) {
-    if (confirm("Vas ha eliminar el item")) {
-      evt.target.parentElement.parentElement.remove();
-      checkUI();
-    }
-  }
-}
-
-function clearItems() {
-  // itemList.innerHTML = "";
-  if (confirm("Vas ha eliminar toda la lista")) {
-    while (itemList.firstChild) {
-      itemList.removeChild(itemList.firstChild);
-    }
-    checkUI();
-  }
-}
-
-function filterItems(evt) {
-  const text = evt.target.value.toLowerCase();
-  const items = itemList.querySelectorAll("li");
-
-  items.forEach((item) => {
-    const itemName = item.firstChild.textContent.toLowerCase();
-
-    if (itemName.includes(text)) {
-      item.style.display = "flex";
-    } else {
-      item.style.display = "none";
-    }
-  });
-}
-
+/**
+ * @description Habilita o desabilita los elementos graficos según si
+ * hay items en la lista o no
+ */
 function checkUI() {
   const items = itemList.querySelectorAll("li");
   if (items.length === 0) {
@@ -102,8 +116,14 @@ function checkUI() {
 }
 
 /***************   Local Storage functions ****************/
-function addItemToLocalStorage(item) {}
-
+function addItemToLocalStorage(item) {
+  //Traer los datos del localStorage
+  const itemsFromLocalStorage = getItemsFromLocalStorage();
+  //añadir el nuevo item al array
+  itemsFromLocalStorage.push(item);
+  //Convertir el array a texto y lo guardamos
+  localStorage.setItem("lista", JSON.stringify(itemsFromLocalStorage));
+}
 function getItemsFromLocalStorage() {
   let itemsFromLocalStorage;
   if (localStorage.getItem("lista") === null) {
@@ -122,11 +142,28 @@ function getItemsFromLocalStorage() {
   return JSON.parse(items);
   */
 }
+function removeItemFromLocalStorage(item) {
+  //Traer los datos del localStorage
+  let itemsFromLocalStorage = getItemsFromLocalStorage();
+  //eliminar el elemento del array
+  itemsFromLocalStorage = itemsFromLocalStorage.filter((i) => i !== item);
+  //Convertir el array a texto y lo guardamos
+  localStorage.setItem("lista", JSON.stringify(itemsFromLocalStorage));
+}
+//funcion que devuelve el indice de un elemento
+//  si exixte en la lista o -1 si nó existe
+function checkIfItemExists(item) {
+  //Traer los datos del localStorage
+  const itemsFromLocalStorage = getItemsFromLocalStorage();
+  //Mira si existe
+  return itemsFromLocalStorage.indexOf(item);
+}
+
+//TODO funcion update
 
 //Event Listeners
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 btnClear.addEventListener("click", clearItems);
 itemFilter.addEventListener("input", filterItems);
-
-checkUI();
+document.addEventListener("DOMContentLoaded", displayItems);
